a4929a6db454cf11a7ebb8cd7b05079654abd13d
Cemil Ibrahimov <surdioti@gmail.com>
2023-12-03 14:56:19 +0100

    Program praca domwa dzien 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "Kobieta";
+int wiek = 20;
+
+if (wiek < 30 && sex == "Kobieta")
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (wiek == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "Mezczezna" && wiek < 18)
+{
+    Console.WriteLine("Nepelnoletni mezczyzna");
+}
